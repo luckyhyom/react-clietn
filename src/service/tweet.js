a27dc75e95bfb55282293c35f792a8da029a3619
luckyhyom <bs_khm@naver.com>
2021-07-21 23:47:55 +0900

    혼자서 연결하기

--- src/service/tweet.js
+++ src/service/tweet.js
@@ -11,9 +11,12 @@ export default class TweetService {
   ];
 
   async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
+    
+    const result = username
+      ? fetch(`http://127.0.0.1:8080/tweets?username=${username}`,{method:'GET'}).then(res=>res.json())
+      : fetch(`http://127.0.0.1:8080/tweets`,{method:'GET'}).then(res=>res.json());
+    
+      return result;
   }
 
   async postTweet(text) {
@@ -23,21 +26,39 @@ export default class TweetService {
       name: 'Ellie',
       username: 'ellie',
       text,
-    };
-    this.tweets.push(tweet);
+    }
+
+    let test = fetch(`http://127.0.0.1:8080/tweets`,{method:'POST',headers:{'Content-Type':'application/json'},body: JSON.stringify(tweet)})
+    .then(res=>res.json());
+
+    console.log(test);
+
     return tweet;
   }
 
   async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+    fetch(`http://127.0.0.1:8080/tweets/${tweetId}`,{method:'DELETE'}).then(res=>res.json())
   }
 
   async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
+    const tweet = await fetch(`http://127.0.0.1:8080/tweets/${tweetId}`,{method:'GET'}).then(res=>res.json());
     if (!tweet) {
       throw new Error('tweet not found!');
     }
     tweet.text = text;
+    let test = await fetch(`http://127.0.0.1:8080/tweets/${tweetId}`,
+      {
+        method:'PUT',
+        headers:{'Content-Type':'apllication/json'},
+        body:JSON.stringify({tweet})
+      }
+    )
+    .then(res=>res.json());
+
     return tweet;
   }
 }
+
+// 외부 api와의 연결이 의미하는 것
+// 나도 모르겠다. 각 서버에서 하나의 DB에 crud하는것과 뭐가 다르지?
+// api를 변경할때 api서버에서만 변경하면 되는 것? => 통일시키는것?
